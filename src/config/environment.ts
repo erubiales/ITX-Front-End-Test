5ad9c386635af54ffff92989ac4f198bca82cd7c
erubiales <vintajeskull98@gmail.com>
2026-06-12 13:46:29 +0200

    Cambios gestion de entorno de desarrollo y produccion, API URL lo gestiona archivo .env

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-    production: false,
-    apiUrl: 'https://itx-frontend-test.onrender.com/',
-    apiTimeout: 5000,
+    production: process.env.NODE_ENV === 'production',
+    apiUrl: process.env.REACT_APP_API_URL || 'https://itx-frontend-test.onrender.com/',
+    apiTimeout: Number(process.env.REACT_APP_API_TIMEOUT) || 5000,
 
     products: 'api/product',
     productDetail: 'api/product/',
